54392a60a907a18a526b0be7e70e90f8f8de708f
Suyarov Bek <suyarov@gmail.com>
2025-08-26 22:10:04 +0500

    6-bosqich

--- ilova.py
+++ ilova.py
@@ -1,12 +1,6 @@
 import sqlite3
+from vazifa import add_vazifa, show_vazifalar, delete_vazifa, get_connection
 
-class Not_id(Exception):
-    pass
-class Vazifa_except(Exception):
-    pass
-
-def get_connection():
-    return sqlite3.connect("info.db")
 
 with get_connection() as conn:
     cur=conn.cursor()
@@ -15,6 +9,7 @@ with get_connection() as conn:
                 matn text)
                 """)
         
+
 while True:
     tanlov=input("""
 1. Vazifa qo'shish
@@ -24,64 +19,6 @@ while True:
 """)
 
 
-
-    def add_vazifa():
-        while True:
-            try:
-                matn=input("Vazifa matnini kiriting:")
-                if not matn.strip():
-                    raise Vazifa_except("Vazifa bo'sh bo'lishi mumkin emas!")
-                with get_connection() as conn:
-                    cur=conn.cursor()
-                    cur.execute("insert into vazifalar(matn) values (?)" ,(matn,))
-                    conn.commit()
-                    print("Vazifa saqlandi!")
-                    break
-            except Exception as e:
-                print(e)
-            
-
-    def show_vazifalar():
-        with get_connection() as conn:
-            cur=conn.cursor()
-            cur.execute("select * from vazifalar")
-            vazifalar=cur.fetchall()
-            print("""
-<<Vazifalar>>""")
-            for id,task in vazifalar:
-                print(f"{id}. {task}") 
-        return None
-
-
-    def delete_vazifa():
-        try:
-            while True:
-                try:
-                    id=input("O'chirmoqchi bo'lgan vazifangiz id sini kiriting:")
-                    if not id.strip():
-                        raise Not_id("ID kiritilmagan!")
-                    elif not id.isdigit():
-                        raise Not_id("Iltimos raqam kiriting!")
-                    break
-
-                except Exception as e:
-                    print(e)
-            
-            with get_connection() as conn:
-                cur=conn.cursor()
-                cur.execute("select id from vazifalar")
-                result=[]
-                for i in cur.fetchall():
-                    result.append(i[0])
-                if not int(id) in result:
-                    raise Not_id("Bu id mavjud emas!")
-                
-                cur.execute("delete from vazifalar where id=?", (id,))
-                conn.commit()
-                print("Vazifa o'chirildi!")
-        except Exception as e:
-            print(e)
-
     if tanlov=='1':
         add_vazifa()
 
@@ -90,6 +27,10 @@ while True:
     
     elif tanlov=='3':
         delete_vazifa()
+    
+    elif tanlov=='0':
+        print("Dasturdan chiqildi!")
+        break
 
 
 
---
+++ vazifa.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+
+class Not_id(Exception):
+    pass
+
+class Vazifa_except(Exception):
+    pass
+
+
+def get_connection():
+    return sqlite3.connect("info.db")
+
+
+def add_vazifa():
+        while True:
+            try:
+                matn=input("Vazifa matnini kiriting:")
+                if not matn.strip():
+                    raise Vazifa_except("Vazifa bo'sh bo'lishi mumkin emas!")
+                with get_connection() as conn:
+                    cur=conn.cursor()
+                    cur.execute("insert into vazifalar(matn) values (?)" ,(matn,))
+                    conn.commit()
+                    print("Vazifa saqlandi!")
+                    break
+            except Exception as e:
+                print(e)
+            
+
+def show_vazifalar():
+    with get_connection() as conn:
+        cur=conn.cursor()
+        cur.execute("select * from vazifalar")
+        vazifalar=cur.fetchall()
+        print("""
+<<Vazifalar>>""")
+        for id,task in vazifalar:
+            print(f"{id}. {task}") 
+    return None
+
+
+def delete_vazifa():
+    try:
+        while True:
+            try:
+                id=input("O'chirmoqchi bo'lgan vazifangiz id sini kiriting:")
+                if not id.strip():
+                    raise Not_id("ID kiritilmagan!")
+                elif not id.isdigit():
+                    raise Not_id("Iltimos raqam kiriting!")
+                break
+
+            except Exception as e:
+                print(e)
+        
+        with get_connection() as conn:
+            cur=conn.cursor()
+            cur.execute("select id from vazifalar")
+            result=[]
+            for i in cur.fetchall():
+                result.append(i[0])
+            if not int(id) in result:
+                raise Not_id("Bu id mavjud emas!")
+            
+            cur.execute("delete from vazifalar where id=?", (id,))
+            conn.commit()
+            print("Vazifa o'chirildi!")
+    except Exception as e:
+        print(e)

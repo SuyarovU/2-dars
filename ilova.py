2bf89f8703f399ad5690bfa8b1afd73fbe264913
Suyarov Bek <suyarov@gmail.com>
2025-08-26 20:27:15 +0500

    2-bosqich

--- ilova.py
+++ ilova.py
@@ -1,7 +1,53 @@
-tanlov=input("""
+import sqlite3
+
+def get_connection():
+    return sqlite3.connect("info.db")
+
+with get_connection() as conn:
+    cur=conn.cursor()
+    cur.execute("""create table if not exists vazifalar(
+                id integer primary key autoincrement,
+                matn text)
+                """)
+        
+while True:
+    tanlov=input("""
 1. Vazifa qo'shish
 2. Ro'yxatni ko'rish
 3. Vazifani o'chirish
 0. Chiqish
-""")
+    """)
+
+
+
+    def add_vazifa():
+        while True:
+            matn=input("Vazifa matnini kiriting:")
+            if matn:
+                with get_connection() as conn:
+                    cur=conn.cursor()
+                    cur.execute("insert into vazifalar(matn) values (?)" ,(matn,))
+                    conn.commit()
+                    print("Vazifa saqlandi!")
+                break
+
+    def show_vazifalar():
+        with get_connection() as conn:
+            cur=conn.cursor()
+            cur.execute("select * from vazifalar")
+            vazifalar=cur.fetchall()
+            for _ in vazifalar:
+                print(_) 
+        return None
+
+    if tanlov=='1':
+        add_vazifa()
+
+    elif tanlov=='2':
+        show_vazifalar()
+
+
+
+
+
 
